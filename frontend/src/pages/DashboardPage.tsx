0987e6e74fe73d192887560913962c0de0852bef
khoi-5 <tranminhkhoichaua5@gmail.com>
2026-08-12 09:34:19 +0700

    update dashboard

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -453,7 +453,7 @@ function DashboardPage({ activeTab }: DashboardPageProps) {
               </div>
             </div>
 
-            <div className="info-card">
+            {/* <div className="info-card">
               <div className="info-icon" style={{ color: '#14b8a6', background: 'rgba(20, 184, 166, 0.1)' }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg>
               </div>
@@ -461,7 +461,7 @@ function DashboardPage({ activeTab }: DashboardPageProps) {
                 <span>ROLE</span>
                 <strong>{user?.role}</strong>
               </div>
-            </div>
+            </div> */}
 
             <div className="info-card">
               <div className="info-icon" style={{ color: '#22c55e', background: 'rgba(34, 197, 94, 0.1)' }}>
